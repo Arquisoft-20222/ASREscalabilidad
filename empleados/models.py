1b3854c6d627ea44d09b8d38c47aee991285e09a
Maria <ma.vargas73@uniandes.edu.co>
2022-09-30 21:24:03 -0500

    empleados

--- empleados/models.py
+++ empleados/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Empleados(models.Model):
+    name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    email = models.CharField(max_length = 255)
+
+
+    def __str__(self):
+        return '%s %s %s' % (self.name, self.last_name, self.email)
